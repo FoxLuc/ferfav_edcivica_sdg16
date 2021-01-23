f710a58f4d033e06b93ab1d880acdb24cb6d6d28
FoxLuc <lucaf8502@gmail.com>
2021-01-23 22:20:39 +0000

    Server MongoDB Inserito

---
+++ sdg16-server/routes/birth.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = 'mongodb+srv://Luca-Ferrari:SDG16_-_ProJect2020@clustersdg16.dnkc2.mongodb.net/ClusterSDG16?retryWrites=true&w=majority';
+
+router.get('/', function (req, res, next) {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("SDG16DB").collection("16.9.1-birth-certification");
+        collection.find({}).toArray((err, result) => {
+            if (err) console.log(err.message);
+            else { res.send(result); console.log(result); }
+            client.close();
+        });
+    });
+});
+
+router.get('/16.9.1-birth-certification/:GeoAreaName', function (req, res, next) {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    GeoAreaName = req.params.GeoAreaName;
+    client.connect(err => {
+        const collection = client.db("SDG16DB").collection("16.9.1-birth-certification");
+        collection.find({ 'GeoAreaName': `${GeoAreaName}` }).toArray((err, result) => {
+            if (err) console.log(err.message);
+            else { res.send(result); console.log(result); }
+            client.close();
+        });
+    });
+});
+
+module.exports = router;
