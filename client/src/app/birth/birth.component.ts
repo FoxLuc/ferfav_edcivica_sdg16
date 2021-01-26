4ad3eef2d729bde6304f5325d442c329e5adfc39
Andrea-Favullo <favullo.andrea.dennis@iisgalvanimi.edu.it>
2021-01-26 10:58:24 +0000

    iniziato componente birth

--- client/src/app/birth/birth.component.ts
+++ client/src/app/birth/birth.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Sdg16ApiService } from '../sdg16-api.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-birth',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BirthComponent implements OnInit {
 
-  constructor() { }
+  query: string;
+  obs: Observable<Object>;
+  dati_nascite: any;
 
-  ngOnInit(): void {
+  constructor(public sdg_service: Sdg16ApiService) { }
+  ngOnInit(): void {  }
+
+  submit(query: HTMLInputElement): void {
+
+    if (!query.value) {
+      return;
+    }
+    this.query = query.value;
+    if (this.query) {
+      console.log(`Il sistema ha considerato (${this.query}) come stringa non vuota`)
+      this.obs = this.sdg_service.getGeoAreaNameBirth(this.query);
+      this.obs.subscribe((data) => { this.dati_nascite = data; console.log(this.dati_nascite) });
+    } else {
+      console.log(`Il sistema ha considerato (${this.query}) come stringa vuota o null`)
+      this.obs = this.sdg_service.getBirth();
+      this.obs.subscribe((data) => { this.dati_nascite = data; console.log(this.dati_nascite) });
+    }
+
   }
 
 }
