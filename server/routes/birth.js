fc307ed287b45bec1aa99a75dd7415e39df5a735
Andrea-Favullo <favullo.andrea.dennis@iisgalvanimi.edu.it>
2021-01-28 10:38:17 +0000

    28-01

--- server/routes/birth.js
+++ server/routes/birth.js
@@ -8,6 +8,7 @@ const uri = 'mongodb+srv://Luca-Ferrari:SDG16_-_ProJect2020@clustersdg16.dnkc2.m
 //ESEMPIO: URL/birth
 router.get('/', function (req, res, next) { //Prende TUTTO 
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    res.setHeader('Access-Control-Allow-Origin', '*');
     client.connect(err => {
         const collection = client.db("SDG16DB").collection("16.9.1-birth-certification"); //Prende dalla collezione
         collection.find({}).toArray((err, result) => { //Prende secondo la query {} quindi nessun criterio
